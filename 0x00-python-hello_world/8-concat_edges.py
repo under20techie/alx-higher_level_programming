5bf0511817ad793eefb7fbcc6ff52b846f105176
Mary Agiamah <maryagiamah@gmail.com>
2023-11-27 04:35:59 -0800

    you

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming"):str.find("object-oriented\
- programming") + len("object-oriented programming")] + "\
- " + str[str.find("with"):str.find("with") + len("with")] + "\
- " + str[str.find("Python"):str.find("Python") + len("Python")] 
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
