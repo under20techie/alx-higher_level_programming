9da9a30b51685a9ef565156c31bbc90bf49838d4
Mary Agiamah <maryagiamah@gmail.com>
2023-11-28 02:45:08 -0800

    linkedlistcyleshowedmeshege

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: The list to check if there's a cycle
+ *
+ * Return: 1 if thers a loop , 0 if none.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list->next, *fast = list->next->next; 
+	if (!list || !list->next)
+		return (0);
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
