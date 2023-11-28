246594949bc1c24eeb0328caaabe27f8450c8a19
Mary Agiamah <maryagiamah@gmail.com>
2023-11-28 07:02:01 -0800

    you

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 num1 = 0
-num2 = 0
-while (num1 < 10):
-    num2 = 0
-    while(num2 < 10):
-        print("{}{}".format(num1, num2), end="")
-        if num1 != 9 or num2 != 9:
-            print(", ", end="")
-        else:
-            print()
-        num2 += 1
+while (num1 < 100):
+    if num1 != 99:
+        print("{:02d}, ".format(num1), end="")
+    else:
+        print("{:02d}".format(num1))
     num1 += 1
