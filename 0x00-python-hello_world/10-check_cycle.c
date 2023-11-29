f5e7a3bc12d3ae0f4f92f83a98fb49aa8d9e3a3c
Mary Agiamah <maryagiamah@gmail.com>
2023-11-29 01:40:35 -0800

    you

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,9 +7,11 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list->next, *fast = list->next->next;
+	listint_t *slow, *fast;
 	if (!list || !list->next)
 		return (0);
+	slow = list->next;
+	fast = list->next->next;
 	while (fast  && fast->next)
 	{
 		if (slow == fast)
