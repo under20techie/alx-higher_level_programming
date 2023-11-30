2df761125c734d31895e30cceeca6db4d4d64f39
under20techie <133390683+under20techie@users.noreply.github.com>
2023-11-30 13:19:55 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     len1 = len(argv)
     if len1 == 2:
-        print("{} argument:".format(len1))
+        print("{} argument:".format(len1 - 1))
     elif len1 > 2:
-        print("{} arguments:".format(len1))
+        print("{} arguments:".format(len1 - 1))
     else:
-        print("{} arguments.".format(len1))
+        print("{} arguments.".format(len1 - 1))
     for i in range(1, len1):
         print("{}: {}".format(i, argv[i]))
