d63e9dd51e440a0fe23d1bda2d94cf227917b634
Mary Agiamah <maryagiamah@gmail.com>
2024-01-15 04:20:44 -0800

    ypu

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Contains list_division """
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    '''Divides element by elemnets in 2 lists '''
+
+    new_list = []
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    return new_list
