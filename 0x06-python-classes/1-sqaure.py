e77cbf73b6fe27ad45a26abd4853eef686b2abe5
Mary Agiamah <maryagiamah@gmail.com>
2024-01-16 01:51:35 -0800

    you

---
+++ 0x06-python-classes/1-sqaure.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains sqaure class """
+
+
+class Square:
+    """ Square class"""
+    def __init__(self, size):
+        """Init Method """
+
+        self.__size = size
