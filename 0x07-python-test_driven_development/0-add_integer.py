01d770e73f2e01cbd666f2b8789c7a15d00732d0
Mary Agiamah <maryagiamah@gmail.com>
2024-01-16 07:30:46 -0800

    you

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add integer """
+
+
+def add_integer(a, b=98):
+    """Return the addition of two numbers."""
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
