d1f936d8f8b28da000f182384f4dcb27cf01bc61
Mary Agiamah <maryagiamah@gmail.com>
2024-01-16 02:36:04 -0800

    you

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Contains sqaure class """
+
+
+class Square:
+    """ Square class"""
+    def __init__(self, size=0):
+        """Init Method """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Size Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Size Getter"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Area Method"""
+
+        return self.__size ** 2        
