280f20e07bdc7ae856809cbfd8910c9b52b384e5
Mary Agiamah <maryagiamah@gmail.com>
2024-01-16 01:10:04 -0800

    you

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Contains safe_print_integer_err """
+import sys
 
 
 def safe_print_integer_err(value):
-    '''Prints an intege '''
+    '''Prints an integer '''
 
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}".format(e))
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
