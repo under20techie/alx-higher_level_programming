5e80b6d0cd5fe03ac640a46ed0f72cb2a554a3d7
Mary Agiamah <maryagiamah@gmail.com>
2024-01-17 07:08:35 -0800

    you

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Contains text_indentation func """
+
+
+def text_indentation(text):
+    """Prints text_indentation """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    flag = 0
+    for c in text.strip():
+        if c in ".?:" and flag == 0:
+            print("{}\n".format(c))
+            flag = 1
+        elif flag:
+            flag = 0
+            continue
+        else:
+            print(c, end='')
+
