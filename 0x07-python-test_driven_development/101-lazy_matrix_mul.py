257d50e0f818157b69631e7ef3d19abe95be008a
Mary Agiamah <maryagiamah@gmail.com>
2024-01-18 02:17:28 -0800

    you

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Matrix mul """
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ matrices mul """
+
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if not m_a:
+        raise ValueError("m_a can't be empty")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    if not m_b:
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        if type(row) is not list:
+            raise TypeError("m_a must be a list of lists")
+        if not row:
+            raise ValueError("m_a can't be empty")
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+        for col in row:
+            if not isinstance(col, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+
+    for row in m_b:
+        if type(row) is not list:
+            raise TypeError("m_b must be a list of lists")
+        if not row:
+            raise ValueError("m_b can't be empty")
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
+        for col in row:
+            if not isinstance(col, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    first_mat = np.array(m_a)
+    sec_mat = np.array(m_b)
+    return np.matmul(first_mat, sec_mat)
