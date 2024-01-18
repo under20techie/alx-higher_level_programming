34903231d2e3f020b15e812c212e16fcbb81e2d4
Mary Agiamah <maryagiamah@gmail.com>
2024-01-18 01:42:46 -0800

    you

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,8 +22,8 @@ def matrix_mul(m_a, m_b):
         if len(row) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
         for col in row:
-            if type(col) is not int and type(col) is not float:
-                TypeError("m_a should contain only integers or floats")
+            if not isinstance(col, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
 
     for row in m_b:
         if type(row) is not list:
@@ -31,15 +31,22 @@ def matrix_mul(m_a, m_b):
         if not row:
             raise ValueError("m_b can't be empty")
         if len(row) != len(m_b[0]):
-            raise TypeError("each row of m_a must be of the same size")
+            raise TypeError("each row of m_b must be of the same size")
         for col in row:
-            if type(col) is not int and type(col) is not float:
-                TypeError("m_b should contain only integers or floats")
+            if not isinstance(col, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    
-    for i in len(m_a):
-        for j in len(m_b[0]):
-            col += m_a[i][j] * m_b[j][i]
-        column_matrix.append[]
+
+    column_matrix = []
+    new_matrix = []
+    for row in m_a:
+        column_matrix = []
+        for i in range(len(m_a[0])):
+            col = 0
+            for j in range(len(m_b)):
+                col += row[j] * m_b[j][i]
+            column_matrix.append(col)
+        new_matrix.append(column_matrix)
+    return new_matrix
