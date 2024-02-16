2a315a85dc6238518016a2d9739f25e7f1e391a3
Your Name <you@example.com>
2024-02-16 19:20:21 +0100

    you

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- calculate max temperature for each state 
-SELECT state, MAX(*) as max_temp FROM temperatures
+SELECT state, MAX(*) AS  max_temp FROM temperatures
 GROUP BY state
 ORDER BY state;
