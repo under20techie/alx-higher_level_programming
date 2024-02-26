cbfd6b80d8bee13d13edab3217f5eb4b1e6ca8cc
Your Name <you@example.com>
2024-02-26 16:59:21 +0100

    3-force_name.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name FROM cities
-WHERE cities.id = (
+SELECT id, name FROM cities
+WHERE id = (
 	SELECT id FROM states
-	WHERE name = Carlifornia
+	WHERE name = California
 )
-ORDER BY cities.id;
+ORDER BY id;
