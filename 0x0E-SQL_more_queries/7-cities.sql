b9bc3af7b3cb75ebf0059db286d690fd8e8d0beb
Your Name <you@example.com>
2024-02-26 16:26:18 +0100

    3-force_name.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,                                     UNIQUE(id),
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
 	FOREIGN KEY(state_id) REFERENCES states(id)
 );
