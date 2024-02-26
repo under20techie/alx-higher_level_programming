95a43b26c35af47117209f13cdb595e6af9e275d
Your Name <you@example.com>
2024-02-26 18:20:21 +0100

    3-force_name.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
