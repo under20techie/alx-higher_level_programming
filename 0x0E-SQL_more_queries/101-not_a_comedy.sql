7b1d555c134633961f2af493a3b520ce76458c1f
Your Name <you@example.com>
2024-02-27 03:18:48 +0100

    3-force_name.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title AS title FROM tv_shows                    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id                                                               INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id  WHERE tv_genres.name = 'Comedy' AND title IS NULL
+ORDER BY title
