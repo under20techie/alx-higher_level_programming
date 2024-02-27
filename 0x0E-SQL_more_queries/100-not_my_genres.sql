b8294e7ad4e4b5d9eb219550ce58655b8757fd24
Your Name <you@example.com>
2024-02-27 13:59:05 +0100

    3-force_name.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,6 @@
 -- Write a script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 SELECT DISTINCT tv_genres.name AS name FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id     WHERE tv_shows.title = 'Dexter' AND tv_show_genres.show_id IS NULL
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title IS NOT 'Dexter'
 ORDER BY name;
