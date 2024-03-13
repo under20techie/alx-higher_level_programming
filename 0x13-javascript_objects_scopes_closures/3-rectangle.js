ed0742da32dfdae7a7502dd60819de6cd39732ca
Mary Agiamah <maryagiamah@gmail.com>
2024-03-13 15:21:14 +0000

    Add print method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    while (i < this.height) {
+      j = 0;
+      while (j < this.width) {
+        process.stdout.write('X');
+        j++;
+      }
+      console.log('');
+      i++;
+    }
+  }
+}
+module.exports = Rectangle;
