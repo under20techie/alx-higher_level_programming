1638ab619c34175fe55c3b2fec916b1ca2bfb56f
Mary Agiamah <maryagiamah@gmail.com>
2024-03-14 10:10:16 +0000

    Fix semistandard errors

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list
-const new_list = list.map((x) => x * list.indexOf(x));
+const list = require('./100-data.js').list;
+const newList = list.map((x) => x * list.indexOf(x));
 console.log(list);
-console.log(new_list);
-
+console.log(newList);
