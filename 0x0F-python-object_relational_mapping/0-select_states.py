589857ed23af1a0b8a2f801a8ff88ae9267e2717
Mary Agiamah <maryagiamah@gmail.com>
2024-03-18 13:26:13 +0000

    Did my-frisrsql

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+username, password, database = sys.argv[1:]
+
+def access_states():
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,passwd=password, db=database)
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    conn.close()
+if __name__ == '__main__':
+    access_states()
