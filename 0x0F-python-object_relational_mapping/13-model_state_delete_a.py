cd0ec971e62c72c31e87ec2ca844bcd3b708192b
Under20techie <under20techie@gmail.com>
2024-04-04 18:59:01 +0100

    delete table entry

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that lists only the first State objects from the database hbtn_0e_6_usa
+
+ """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+
+def fetch_update():
+    """Func where we fetch states from database """
+
+    username, passwd, db = sys.argv[1:]
+
+    # create connect to database
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{passwd}@localhost/{db}',
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+
+    # create session for transaction
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query database to in order to delete row names containing a
+    result = (
+            session.query(State)
+            .filter(State.name.like('%a%'))
+            .delete()
+        )
+
+    # close session
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    fetch_update()
