47f48f35b56a18343b1d02c7b47b4fe138130eb3
Under20techie <under20techie@gmail.com>
2024-04-04 15:11:30 +0100

    Return only  first row of query result

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Script that lists only the first State objects from the database hbtn_0e_6_usa
+
+ """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+
+def state_fetch_first():
+    """Func where we fetch states from database """
+
+    username, passwd, db = sys.argv[1:]
+
+    # create connect to database
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{passwd}@localhost/{db}',
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+
+    # create session for transaction
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create query to database
+    state = session.query(State).order_by(State.id).first()
+
+    # Print result
+    if state is None:
+        print()
+    else:
+        print(f"{state.id}: {state.name}")
+
+    # close session
+    session.close()
+
+
+if __name__ == '__main__':
+    state_fetch_first()
