5ae2218e30080b4ddd8c6fed3573b2d31afeb553
Under20techie <under20techie@gmail.com>
2024-04-05 15:02:50 +0100

    state and cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Script that lists only the first State objects from the database hbtn_0e_6_usa
+
+ """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+
+
+def state_city_fetch():
+    """Func where we fetch states from database """
+
+    username, passwd, db = sys.argv[1:]
+
+    # create connect to database
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{passwd}@localhost/{db}',
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+
+    # create session for transaction
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create query to database
+    result = (
+            session.query(State)
+            .join(City)
+            .order_by(State.id, City.id)
+        )
+
+    # Print result
+    for state in result:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
+
+    # close session
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    state_city_fetch()
